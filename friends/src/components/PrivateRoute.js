99bab484abf791c066e11a84291a4e6f49922f15
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-03 23:50:03 -0500

    added PrivateRoute.js component

---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRout = ({component: Component, ...rest}) => {
+    return(
+        <Route
+            {...rest}
+            render={
+                props => {
+                    if(localStorage.getItem('token'))
+                        <Component {...props} />
+                    else
+                        <Redirect path='/' />
+                }
+            }
+        />
+    )
+}
+
+export default PrivateRout;
